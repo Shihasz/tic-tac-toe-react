3faa5ede983747b2137c1a4e980ee40c88a37f30
Shihasz <shihaspkdst@gmail.com>
2025-05-20 11:29:32 +0530

    removes redundant state and disable clicked button

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,44 +1,48 @@
-
 const initialGameBoard = [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
-export default function GameBoard ({ onSelectSquare, turns }) {
-    // const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  // const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
 
-    // function handleSelectSquare(rowIndex,colIndex) {
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     });
-    //     onSelectSquare();
-    // }
+  // function handleSelectSquare(rowIndex,colIndex) {
+  //     setGameBoard((prevGameBoard) => {
+  //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+  //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //         return updatedBoard;
+  //     });
+  //     onSelectSquare();
+  // }
 
-    let gameBoard=initialGameBoard;
+  let gameBoard = initialGameBoard;
 
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
 
-        gameBoard[row][col] = player;
-    }
+    gameBoard[row][col] = player;
+  }
 
-    return (
-        <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
-                <li key={rowIndex}>
-                    <ol>
-                        {row.map((playerSymbol, colIndex) => (
-                        <li key={colIndex}>
-                            <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
-                        </li>
-                        ))}
-                    </ol>
-                </li>
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol != null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
             ))}
-        </ol>
-    )
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
